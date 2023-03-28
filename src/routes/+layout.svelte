5ec5f830e0b2bb204302840fac301973b347c8ab
trongnguyen24 <trongnguyen24@gmail.com>
2023-03-28 15:42:41 +0700

    1st

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Header from './Header.svelte';
+	import '../app.css';
+	import bgimg from '$lib/images/bg.webp';
+	import bgimg_fallback from '$lib/images/bg.jpg';
+	/** @type {import('./$types').LayoutData} */
+	export let data;
+</script>
+
+<div class="app">
+	<Header />
+	<div class="flex flex-col min-h-full">
+		<!-- beam background -->
+		<div
+			class="absolute z-10 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none select-none"
+		>
+			<div class="w-[106.25rem] flex-none justify-center">
+				<picture>
+					<source srcset={bgimg} type="image/webp" />
+					<img src={bgimg_fallback} alt="BG" />
+				</picture>
+			</div>
+		</div>
+
+		<main class="w-full grow mx-auto z-10 pt-28">
+			<slot />
+		</main>
+	</div>
+</div>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<nav
+	class="z-20 fixed w-full max-w-screen-2xl mx-auto bg-white bg-opacity-50 backdrop-blur-sm justify-between border-b border-white items-center px-4 h-14 flex tems-center"
+>
+	<div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-8 h-8"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+			/>
+		</svg>
+	</div>
+	<ul class="flex gap-8">
+		<li>Project</li>
+		<li>Docs</li>
+	</ul>
+</nav>
